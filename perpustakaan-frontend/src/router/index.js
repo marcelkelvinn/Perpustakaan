ebce4d49976eb149695db1698aa2be8eec97a104
Dwi Rama <dwirama0977@gmail.com>
2024-11-24 22:57:27 +0700

    route frontend

--- perpustakaan-frontend/src/router/index.js
+++ perpustakaan-frontend/src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import BukuList from "../components/BukuList.vue";
+import PeminjamanList from "../components/PeminjamanList.vue";
+import DendaList from "../components/DendaList.vue";
+import SirkulasiList from "../components/SirkulasiList.vue";
 
 const routes = [
   {
     path: "/bukus",
     name: "BukuList",
     component: BukuList,
+  },
+  {
+    path: "/peminjamans",
+    name: "PeminjamanList",
+    component: PeminjamanList,
+  },
+  {
+    path: "/dendas",
+    name: "DendaList",
+    component: DendaList,
+  },
+  {
+    path: "/sirkulasis",
+    name: "SirkulasiList",
+    component: SirkulasiList,
   },
 ];
 

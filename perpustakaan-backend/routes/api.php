91d5304ed4ab7ce019798c24d35a5f323ba3ffd9
Dwi Rama <dwirama0977@gmail.com>
2024-12-13 22:38:04 +0700

    none

--- perpustakaan-backend/routes/api.php
+++ perpustakaan-backend/routes/api.php
@@ -21,27 +21,6 @@ Route::prefix('buku')->group(function () {
 });
 
 Route::prefix('denda')->group(function () {
-<<<<<<< HEAD
-    Route::post('/', [DendaService::class, 'create']);
-    Route::get('/', [DendaService::class, 'getAll']);
-    Route::get('/peminjaman/total', [PeminjamanService::class, 'getTotalPeminjaman']);
-    Route::get('{id}', [DendaService::class, 'getById']);
-    Route::put('{id}', [DendaService::class, 'update']);
-    Route::delete('{id}', [DendaService::class, 'delete']);
-    Route::get('/peminjam', [PeminjamanService::class, 'getAll']);
-});
-
-Route::prefix('peminjaman')->group(function () {
-    Route::post('/', [PeminjamanService::class, 'create']);
-    Route::get('/', [PeminjamanService::class, 'getAll']);
-    Route::get('/denda/total', [DendaService::class, 'getTotalDenda']);
-    Route::get('{id}', [PeminjamanService::class, 'getById']);
-    Route::put('{id}', [PeminjamanService::class, 'update']);
-    Route::delete('{id}', [PeminjamanService::class, 'delete']);
-    // Route::get('/users', [UserService::class, 'getAll']);
-    Route::get('/bukus', [BukuService::class, 'getAll']);
-
-=======
     Route::post('/', [DendaService::class, 'create'])->middleware('auth:sanctum');
     Route::put('{id}', [DendaService::class, 'update'])->middleware('auth:sanctum');
     Route::delete('{id}', [DendaService::class, 'delete'])->middleware('auth:sanctum');
@@ -51,7 +30,6 @@ Route::prefix('peminjaman')->group(function () {
     Route::post('/', [PeminjamanService::class, 'create'])->middleware('auth:sanctum');
     Route::put('{id}', [PeminjamanService::class, 'update'])->middleware('auth:sanctum');
     Route::delete('{id}', [PeminjamanService::class, 'delete'])->middleware('auth:sanctum');
->>>>>>> upstream/main
 });
 
 Route::prefix('sirkulasi')->group(function () {
